e191ac3f6ab62341cf52cb07f44542de4bb4e760
Evan Ó Catháin <Evan.Keane@gmail.com>
2020-01-29 08:49:59 +0000

    Tweak to check for CHIME's periodic FRB
    Tweak to check for CHIME's periodic FRB from this paper: https://arxiv.org/abs/2001.10275

---
+++ README.md
@@ -0,0 +1,5 @@
+Run the following to get chime_period.png
+
+python getper.py -i CHIME_seconds -p1 86400.0 -p2 8640000.0 -maxdiff 86400000.0 -pstep 8640.0 -days 1
+
+run with a -h to see what the options are.
--- getper.py
+++ getper.py
@@ -13,9 +13,10 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-i', dest='input_file', help='set the input file name (default: file)', default="file")
 parser.add_argument('-p1', type=float, dest='p1', help='set the lowest period (default: 0.1 seconds)', default=0.100)
 parser.add_argument('-p2', type=float, dest='p2', help='set the lowest period (default: 10.0 seconds)', default=10.0)
+parser.add_argument('-days', dest='days', help='plot output in days rather than seconds (default: seconds)', default=False)
 parser.add_argument('-maxdiff', type=float, dest='maxdiff', help='maximum time difference to use (default: 3600.0 seconds)', default=3600.0)
 parser.add_argument('-mjd', dest='mjd', help='flag input times as MJD rather than seconds (default: false)', default=False)
-parser.add_argument('-pstep', dest='pstep', help='trial period step size in milliseconds (default: 1.0)', default=1.0)
+parser.add_argument('-pstep', type=float, dest='pstep', help='trial period step size in seconds (default: 0.001)', default=0.001)
 parser.add_argument('-tol', dest='tol', help='percentage tolerance in phase to count as a match (default: 10.0)', default=10.0)
 parser.add_argument('--version', action='version', version='%(prog)s 0.0.3')
 args = parser.parse_args()
@@ -36,7 +37,8 @@ if mjd:
 
 ## Get the time differences
 maxdiff = args.maxdiff
-pstep = args.pstep*0.001
+#pstep = args.pstep*0.001
+pstep = args.pstep
 diffs = np.zeros(ntimes*ntimes)
 k = 0
 for i in range(0,ntimes):
@@ -56,7 +58,7 @@ print diffs_unique
 ## Search period range
 p1 = args.p1
 p2 = args.p2
-pstep = 0.001
+#pstep = 0.001
 nsteps = int((p2 - p1)/pstep)
 print "Searching from",p1,"to",p2,"in",nsteps,"steps"
 
@@ -73,6 +75,11 @@ for i in range(0,nsteps):
 
 ## Plot the matches
 plt.ylabel("Number of matches")
-plt.xlabel("Trial Period (s)")
-plt.plot(periods,matches)
+days=args.days
+if args.days:
+    plt.xlabel("Trial Period (days)")
+    plt.plot(periods/86400.0,matches)
+else:
+    plt.xlabel("Trial Period (s)")
+    plt.plot(periods,matches)
 plt.show()
