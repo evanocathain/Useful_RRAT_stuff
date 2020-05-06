dbf914fc6bb1cedd42284b748075477b50397ed3
Fabian Jankowski <fjankowsk@gmail.com>
2020-05-06 11:21:23 +0100

    Make it work with python 3 and improve the code formatting.

--- getper.py
+++ getper.py
@@ -24,25 +24,25 @@ args = parser.parse_args()
 ## Read in data
 # Read in barycentred times
 input_file = args.input_file
-print "Reading input from file:",input_file
+print("Reading input from file: {}".format(input_file))
 times = np.genfromtxt(input_file)
 times.sort()
 ntimes = np.size(times)
-print "Total number of pulse times:",ntimes
-print "Total number of unique time differences:",ntimes*(ntimes-1)/2
+print("Total number of pulse times: {0}".format(ntimes))
+print("Total number of unique time differences: {0}".format(ntimes * (ntimes - 1) / 2.0))
 # If input times in MJD, convert to seconds
 mjd = args.mjd
 if mjd:
-    times = (times-int(times[0]))*86400.0
+    times = (times - int(times[0])) * 86400.0
 
 ## Get the time differences
 maxdiff = args.maxdiff
-#pstep = args.pstep*0.001
+#pstep = args.pstep * 0.001
 pstep = args.pstep
-diffs = np.zeros(ntimes*ntimes)
+diffs = np.zeros(ntimes * ntimes)
 k = 0
-for i in range(0,ntimes):
-    for j in range(0,ntimes):
+for i in range(0, ntimes):
+    for j in range(0, ntimes):
         diff = times[j] - times[i]
         if ( np.abs(diff) <= maxdiff):
             if ( diff < 0.0 ):
@@ -50,36 +50,49 @@ for i in range(0,ntimes):
             else:
                 diffs[k] = diff
             k = k+1
+
 diffs.sort()
 diffs_unique = np.unique(diffs)
-print "Total number of unique time differences less than ",maxdiff," seconds:",np.size(diffs_unique)
-print diffs_unique
+print("Total number of unique time differences less than {0} seconds: {1}".format(
+    maxdiff,
+    np.size(diffs_unique)
+)
+)
+print(diffs_unique)
 
 ## Search period range
 p1 = args.p1
 p2 = args.p2
 #pstep = 0.001
-nsteps = int((p2 - p1)/pstep)
-print "Searching from",p1,"to",p2,"in",nsteps,"steps"
+nsteps = int((p2 - p1) / pstep)
+print("Searching from {0} to {1} in {2} steps".format(
+    p1,
+    p2,
+    nsteps
+    )
+)
 
 # Number of matches within 10 percent
-phase_tol = args.tol*0.01
+phase_tol = args.tol * 0.01
 periods = np.zeros(nsteps)
 matches = np.zeros(nsteps)
-for i in range(0,nsteps):
-    periods[i] = p1+i*pstep
-    for j in range(0,np.size(diffs_unique)):
-        phase = (diffs_unique[j]/periods[i]) - int(diffs_unique[j]/periods[i])
-        if ( (phase < 0.5*phase_tol) or (phase > (1.0-0.5*phase_tol))):
+
+for i in range(0, nsteps):
+    periods[i] = p1 + i * pstep
+    for j in range(0, np.size(diffs_unique)):
+        phase = (diffs_unique[j] / periods[i]) - int(diffs_unique[j] / periods[i])
+        if ( (phase < 0.5 * phase_tol) or (phase > (1.0 - 0.5 * phase_tol))):
             matches[i] = matches[i] + 1
 
 ## Plot the matches
 plt.ylabel("Number of matches")
-days=args.days
+days = args.days
+
 if args.days:
     plt.xlabel("Trial Period (days)")
-    plt.plot(periods/86400.0,matches)
+    plt.plot(periods / 86400.0, matches)
 else:
     plt.xlabel("Trial Period (s)")
-    plt.plot(periods,matches)
+    plt.plot(periods, matches)
+
 plt.show()
